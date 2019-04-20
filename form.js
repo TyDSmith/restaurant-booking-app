89fad42f3f24e85f0db69d14264604b19b6a331d
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-04-20 11:02:33 -0700

    js form file creation

---
+++ form.js
@@ -0,0 +1,15 @@
+
+var restarauntArray = [];
+
+
+function submitFormInfo(){
+
+   var name = $('#form-name').val();
+   var email = $('#form-email').val();
+   var phone = $('#form-phone').val();
+
+
+   console.log(name, email, phone);
+
+
+}
