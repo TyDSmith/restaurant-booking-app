03dd4d6af10bbd9779cba93dfd5c3d90fb8cf6dd
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-04-20 11:07:04 -0700

    add new reservations to array

--- form.js
+++ form.js
@@ -7,9 +7,16 @@ function submitFormInfo(){
    var name = $('#form-name').val();
    var email = $('#form-email').val();
    var phone = $('#form-phone').val();
+   var partySize = $('#form-party-size').val();
 
+   var newReservation = {
+       name : name,
+       email : email,
+       phone : phone,
+       partySize : partySize
+   };
 
-   console.log(name, email, phone);
-
-
+   restarauntArray.push(newReservation);
+   
+   console.log(restarauntArray)
 }
